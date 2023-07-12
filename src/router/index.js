c5b8aa945aa405c227dcea895492c62e56ac459f
HaneulSky <tanya0610@naver.com>
2023-07-13 01:30:20 +0300

    changed router file

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter } from 'vue-router'
 import PostsList from '../components/postsList.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  // history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
